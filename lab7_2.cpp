e5f59d8ce283559cf4bca5bf77829627daf223ec
Lica <Tanapatlica@gmail.com>
2022-12-13 15:54:40 +0700

    lap7.2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,31 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, P[5000], k;
+	
 	while(i < 5000){
 		P[i] = 2*i+1;
 		i++;
 	}
+
+	cout << "Please input k: ";
+	cin >> k;
 	
+	if(k>0){
+		int j = 0;
+		int result = 0;
+		while (j<5000){
+			if (P[j]%k==0){
+				result = result - P[j];
+			}else{
+				result = result + P[j];
+			}
+		j++;
+		}
+	cout << "Result = " << result;
+	}else{
+		cout << "Invalid input!!!";
+	}
+
 	return 0;
 }
